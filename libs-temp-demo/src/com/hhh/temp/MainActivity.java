095ecd14019d11fbd0518dfb2467ee991a4231ed
sb3h <470441372@qq.com>
2016-09-12 09:31:52 +0800

    commit template

---
+++ libs-temp-demo/src/com/hhh/temp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hhh.temp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+/**
+ * Sample creating 1 webviews.
+ */
+public class MainActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        
+        setContentView(R.layout.activity_main);
+        
+        
+    }
+}
